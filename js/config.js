3e9904005333d780e5512c529e66a0d593cbc5ca
AlissonMartinsDev <nossila102009@hotmail.com>
2021-02-15 20:25:28 -0300

    Jogo da Cobrinha Recriado com controle de dificuldade e nickname

---
+++ js/config.js
@@ -0,0 +1,28 @@
+function nivel(){
+	let x = document.getElementById('nivel-jogo').value;
+	let nivel = parseInt(x);
+
+	if(x == 150){
+		document.getElementById('dificuldade').innerHTML = 'Fácil';
+	}else if (x == 100){
+		document.getElementById('dificuldade').innerHTML = 'Médio';
+	}else {
+		document.getElementById('dificuldade').innerHTML = 'Difícil';
+	}
+	return nivel;
+};
+function config(){
+	let nickname = document.getElementById('nickname').value;
+	document.getElementById('jogador').innerHTML = nickname;
+
+	let container = document.getElementById('geral');
+	container.style.display = 'flex';
+
+	let painel = document.getElementById('painel-jogo');
+	painel.style.display = 'none';
+};
+function ComecarJogo(){
+	config();
+	let number = nivel();
+	let jogo = setInterval(iniciarJogo, number);
+};
